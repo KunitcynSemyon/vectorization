e78f60ed5888a0232024963a926526d17761799c
KunitcynSemyon <semen.kunicyn@gmail.com>
2020-11-18 23:47:10 +0400

    update

--- homepage/test.py
+++ homepage/test.py
@@ -16,35 +16,74 @@ text_list_category1 = []
 text_list_category2 = []
 text_list_category3 = []
 
-def get_text_category1():
-	for i in range(1, sheet.max_row + 1):
-		if sheet.cell(row=i, column=2).value == 'Категория 1':
-			text_list_category1.append(sheet.cell(row=i, column=1).value)
-	return text_list_category1
+get_data1_from_xlsx = []
+get_data2_from_xlsx = []
+get_data3_from_xlsx = []
 
-def get_text_category2():
-	for i in range(1, sheet.max_row + 1):
-		if sheet.cell(row=i, column=2).value == 'Категория 2':
-			text_list_category2.append(sheet.cell(row=i, column=1).value)
-	return text_list_category2
 
-def get_text_category3():
-	for i in range(1, sheet.max_row + 1):
-		if sheet.cell(row=i, column=2).value == 'Категория 3':
-			text_list_category3.append(sheet.cell(row=i, column=1).value)
-	return text_list_category3
+for i in range(1, sheet.max_row + 1):
+	if sheet.cell(row=i, column=2).value == 'Категория 1':
+		text_list_category1.append(sheet.cell(row=i, column=1).value)
 
-def cat1_tsne(cat1 = get_text_category1()):
+for i in range(1, sheet.max_row + 1):
+	if sheet.cell(row=i, column=2).value == 'Категория 2':
+		text_list_category2.append(sheet.cell(row=i, column=1).value)
+
+for i in range(1, sheet.max_row + 1):
+	if sheet.cell(row=i, column=2).value == 'Категория 3':
+		text_list_category3.append(sheet.cell(row=i, column=1).value)
+
+
+def cat1_tsne(cat1 = text_list_category1):
 	vect1 = CountVectorizer(encoding='koi8r', stop_words = stopWords).fit_transform(cat1)
 	vect1_embedded = TSNE(n_components=2).fit_transform(vect1)
 	return vect1_embedded.tolist()
 
-def cat2_tsne(cat2 = get_text_category2()):
+def cat2_tsne(cat2 = text_list_category2):
 	vect2 = CountVectorizer(encoding='koi8r', stop_words = stopWords).fit_transform(cat2)
 	vect2_embedded = TSNE(n_components=2).fit_transform(vect2)
 	return vect2_embedded.tolist()
 
-def cat3_tsne(cat3 = get_text_category3()):
+def cat3_tsne(cat3 = text_list_category3):
 	vect3 = CountVectorizer(encoding='koi8r', stop_words = stopWords).fit_transform(cat3)
 	vect3_embedded = TSNE(n_components=2).fit_transform(vect3)
 	return vect3_embedded.tolist()
+
+def getData1():
+	for i in range(len(text_list_category1)):
+		coordinates = cat1_tsne()[i]
+		text = text_list_category1[i]
+		category = 'Категория 1'
+		get_data1_from_xlsx.append(
+			{
+				'coordinates' : coordinates,
+				'text' : text,
+				'category' : category,
+			})
+	return get_data1_from_xlsx
+
+def getData2():
+	for i in range(len(text_list_category2)):
+		coordinates = cat2_tsne()[i]
+		text = text_list_category2[i]
+		category = 'Категория 2'
+		get_data2_from_xlsx.append(
+			{
+				'coordinates' : coordinates,
+				'text' : text,
+				'category' : category,
+			})
+	return get_data2_from_xlsx
+
+def getData3():
+	for i in range(len(text_list_category3)):
+		coordinates = cat3_tsne()[i]
+		text = text_list_category3[i]
+		category = 'Категория 3'
+		get_data3_from_xlsx.append(
+			{
+				'coordinates' : coordinates,
+				'text' : text,
+				'category' : category,
+			})
+	return get_data3_from_xlsx
--- homepage/views.py
+++ homepage/views.py
@@ -1,22 +1,33 @@
 from django.shortcuts import render
-from .test import cat1_tsne, cat2_tsne, cat3_tsne, get_text_category1, get_text_category2, get_text_category3
+from .test import cat1_tsne, cat2_tsne, cat3_tsne, getData1, getData2, getData3
+
+coord1 = []
+text1 = []
+for i in range(len(getData1())):
+	coord1.append(getData1()[i]['coordinates'])
+	text1.append(getData1()[i]['text'])
+coord2 = []
+text2 = []
+for i in range(len(getData2())):
+	coord2.append(getData2()[i]['coordinates'])
+	text2.append(getData2()[i]['text'])
+coord3 = []
+text3 = []
+for i in range(len(getData3())):
+	coord3.append(getData3()[i]['coordinates'])
+	text3.append(getData3()[i]['text'])
 
 def index(request):
 	templates = 'index.html'
 
-	text1 = get_text_category1()
-	text2 = get_text_category2()
-	text3 = get_text_category3()
-
-
-
 	context = {
-			'qwe': cat1_tsne(),
-			'asd': cat2_tsne(),
-			'zxc': cat3_tsne(),
+			'coord1': coord1,
+			'coord2': coord2,
+			'coord3': coord3,
 			'text1' : text1,
 			'text2' : text2,
 			'text3' : text3,
+
 		}
 	return render(request, templates, context)
 	
